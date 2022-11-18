efbde566d48c2d70dbdad90ed05b8f675ba50f71
megikapo18 <115168358+megikapo18@users.noreply.github.com>
2022-11-18 17:36:39 +0100

    first commit

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -21,3 +21,46 @@ def create_user():
 def profile():
     allUser = User.getAllUsers()
     return render_template ('profilePage.html', users = allUser)
+
+
+
+@app.route('/delete/<int:id>')
+def deleteuser(id):
+    data={
+        'id':id
+    }
+    User.delete_user(data)
+    return redirect('/profilePage')
+
+
+@app.route('/users/<int:id>')
+def showuser(id):
+    data={
+        'id':id
+    }
+    user= User.get_user_by_id(data)
+    return render_template('data.html',user=user)
+
+@app.route('/users/<int:id>/edit')
+def edit(id):
+    data={
+        'id':id
+    }
+    user= User.get_user_by_id(data)
+    return render_template('users_data_edit.html',user=user)
+
+
+@app.route('/users/<int:id>/update', methods=['POST'])
+def update_user(id):
+    data={
+        'name':request.form['name'],
+        'lastName':request.form['lastName'],
+        'email':request.form['email'],
+        'id':id
+    }
+    User.update_user(data)
+    return redirect('/users/'+str(id))
+
+@app.route('/home')
+def home():
+    return render_template('profilePage.html')
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -25,3 +25,23 @@ class User:
         query="INSERT INTO users (name, lastName, email) VALUES (%(name)s,%(lastName)s,%(email)s);"
         return connectToMySQL(cls.db_name).query_db(query,data)
     
+    @classmethod
+    def delete_user(cls,data):
+        query="DELETE FROM users WHERE id=%(id)s"
+        return connectToMySQL(cls.db_name).query_db(query,data)
+
+    
+
+    @classmethod
+    def get_user_by_id(cls,data):
+        query="SELECT * FROM users WHERE id=%(id)s"
+        results = connectToMySQL(cls.db_name).query_db(query,data)
+        return results[0]
+    
+    @classmethod
+    def update_user(cls,data):
+        query="UPDATE users SET name=%(name)s, lastName=%(lastName)s, email=%(email)s WHERE id=%(id)s"
+        results = connectToMySQL(cls.db_name).query_db(query,data)
+        return results
+
+        
